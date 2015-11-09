1ce24b22e501a2071e480877fb7f16684c886a8e
Michael <mbg@outlook.com>
2015-11-09 12:00:53 +0000

    Update screener.js

--- screener.js
+++ screener.js
@@ -1,1 +1,7 @@
+var request = require('request');
 
+request('http://www.treasury.gov/ofac/downloads/t11sdn.pdf', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+})

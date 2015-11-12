ef68a1c03f1efd6c5343fe957761b357038e11e4
Michael <mbg@outlook.com>
2015-11-12 10:34:00 +0000

    Update screener.js

--- screener.js
+++ screener.js
@@ -3,7 +3,6 @@ var pdf = require('pdf2json');
 
 request('http://www.treasury.gov/ofac/downloads/t11sdn.pdf', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log(body); 
         console.log(pdf.parseBuffer(body));
   }
 })
